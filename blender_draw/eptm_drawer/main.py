9cbfc4c7523bc8aa6bb10c34bd6dbe5793e1ae62
Guillaume Gay <guillaume@mitotic-machine.org>
2014-09-17 20:03:44 +0200

    WIP working on animating junctions with armatures

--- blender_draw/eptm_drawer/main.py
+++ blender_draw/eptm_drawer/main.py
@@ -16,12 +16,26 @@ from . import utils
 from . import graph_io
 from . import objects
 
-def out_neighbours(cell_idx, ctojs_df, jvs_df):
+def out_neighbours(vert_idx, vertices_df, edges_df):
     '''returns the chunk of the vertices dataframe containing the data
-    for the cell's neighbours
+    for the vertex's out neighbours
     '''
-    neighb_idx = ctojs_df.xs(cell_idx, level='source').index.set_names(['vertex_index', 'stamp'])
-    return jvs_df.loc[neighb_idx]
+    try:
+        neighb_idx = edges_df.xs(vert_idx, level='source').index.set_names(['vertex_index', 'stamp'])
+    except KeyError:
+        return None
+    return vertices_df.loc[neighb_idx]
+
+
+def out_edges(vert_idx, vertices_df, edges_df):
+    '''returns the chunk of the edges dataframe containing the data
+    for the vertex's out edges
+    '''
+    try:
+        out_edges = edges_df.xs(vert_idx, level='source').index.set_names(['vertex_index', 'stamp'])
+    except KeyError:
+        return None
+    return out_edges
 
 
 
@@ -33,64 +47,66 @@ def main():
 
     scene = utils.clear_scene()
 
-
-    vertices_df, edges_df = graph_io.import_graph(fname)
-    cells_df = vertices_df[vertices_df['is_cell_vert'] == 1].swaplevel(0, 1).sortlevel()
-    jvs_df = vertices_df[vertices_df['is_cell_vert'] == 0].swaplevel(0, 1).sortlevel()
-    jes_df = edges_df[edges_df['is_junction_edge'] == 1].swaplevel(0,1).swaplevel(1,2).sortlevel()
-    ctojs_df = edges_df[edges_df['is_junction_edge'] == 0].swaplevel(0,1).swaplevel(1,2).sortlevel()
-
-    # is_cell = graph.vertex_properties['is_cell_vert']
-    # is_alive = graph.vertex_properties['is_alive']
-    # is_junction = graph.edge_properties['is_junction_edge']
-    # ixs = graph.vertex_properties['ixs']
-    # wys = graph.vertex_properties['wys']
-    # zeds = graph.vertex_properties['zeds']
-
     cell_color = '#2887c8'
     edge_color = '#3dc828'
-    for cell_idx, cell_df in cells_df.groupby(level='vertex_index'):
-        init_stamp = cell_df.index.get_level_values('stamp')[0]
-        init_cell = cell_df.iloc[0]
 
-        if not init_cell.is_alive:
-            continue
-        name = 'cell{}'.format(cell_idx)
-        neighbs_df = out_neighbours(cell_idx, ctojs_df, jvs_df).xs(init_stamp, level='stamp')
-        if neighbs_df.shape[0] == 0:
-            continue
-        coords = ['ixs', 'wys', 'zeds']
-        c_x, c_y, c_z =  init_cell[coords]
-        j_xx, j_yy, j_zz = neighbs_df[coords].values.T
-        rel_xx = j_xx - c_x
-        rel_yy = j_yy - c_y
-        rel_zz = j_zz - c_z
-        rel_ss = np.arctan2(rel_yy, rel_xx) * np.hypot(rel_yy, rel_xx)
-        phis = np.arctan2(rel_zz, rel_ss)
-        indices = np.argsort(phis)
-        rel_xx = rel_xx[indices]
-        rel_yy = rel_yy[indices]
-        rel_zz = rel_zz[indices]
-        cell_obj = objects.set_cell(name, c_x, c_y, c_z,
-                                    rel_xx, rel_yy, rel_zz, cell_color)
-    # for je in graph.edges():
-    #     if not is_junction[je]: continue
-    #     v0, v1 = je
-    #     name = 'je'+str(v0)+'to'+str(v1)
-    #     je_obj = objects.set_junction(name,
-    #                                   (ixs[v0], ixs[v1]),
-    #                                   (wys[v0], wys[v1]),
-    #                                   (zeds[v0], zeds[v1]), edge_color)
-    for je_idx, je_df in jes_df.groupby(level=['source', 'target']):
-        src_idx, trgt_idx = je_idx
-        init_stamp = je_df.index.get_level_values(level='stamp')[0]
+
+    vertices_df, edges_df = graph_io.import_graph(fname)
+    vertices_df = vertices_df.swaplevel(0, 1).sortlevel()
+    edges_df = edges_df.swaplevel(0,1).swaplevel(1,2).sortlevel()
+    cells_df = vertices_df[vertices_df['is_cell_vert'] == 1]
+    jvs_df = vertices_df[vertices_df['is_cell_vert'] == 0]
+    jes_df = edges_df[edges_df['is_junction_edge'] == 1]
+    ctojs_df = edges_df[edges_df['is_junction_edge'] == 0]
+
+
+    # for cell_idx, cell_df in cells_df.groupby(level='vertex_index'):
+    #     init_stamp = cell_df.index.get_level_values('stamp')[0]
+    #     init_cell = cell_df.iloc[0]
+
+    #     if not init_cell.is_alive:
+    #         continue
+    #     name = 'cell{}'.format(cell_idx)
+    #     neighbs_df = out_neighbours(cell_idx, ctojs_df, jvs_df).xs(init_stamp, level='stamp')
+    #     if neighbs_df.shape[0] == 0:
+    #         continue
+    #     coords = ['ixs', 'wys', 'zeds']
+    #     c_x, c_y, c_z =  init_cell[coords]
+    #     j_xx, j_yy, j_zz = neighbs_df[coords].values.T
+    #     rel_xx = j_xx - c_x
+    #     rel_yy = j_yy - c_y
+    #     rel_zz = j_zz - c_z
+    #     rel_ss = np.arctan2(rel_yy, rel_xx) * np.hypot(rel_yy, rel_xx)
+    #     phis = np.arctan2(rel_zz, rel_ss)
+    #     indices = np.argsort(phis)
+    #     rel_xx = rel_xx[indices]
+    #     rel_yy = rel_yy[indices]
+    #     rel_zz = rel_zz[indices]
+    #     cell_obj = objects.set_cell(name, c_x, c_y, c_z, init_stamp,
+    #                                 rel_xx, rel_yy, rel_zz, cell_color)
+
+    for (jv_idx, stamp), jv_df in jvs_df.iterrows():
+
+        name = 'jv{}'.format(jv_idx)
+        objects.set_jv(name, stamp,
+                       jv_df.ixs, jv_df.wys, jv_df.zeds)
+
+    for je_idx, je_df in jes_df.iterrows():
+        src_idx, trgt_idx, stamp = je_idx
+        #stamp = je_df.index.get_level_values(level='stamp')[0]
         name = 'je{}to{}'.format(src_idx, trgt_idx)
-        j_src = jvs_df.xs((src_idx, init_stamp))
-        j_trgt = jvs_df.xs((trgt_idx, init_stamp))
-        je_obj = objects.set_junction(name,
-                                      (j_src.ixs, j_trgt.ixs),
-                                      (j_src.wys, j_trgt.wys),
-                                      (j_src.zeds, j_trgt.zeds), edge_color)
+        je_obj = objects.set_junction_arm(name, src_idx, trgt_idx)
+        # break
+        # j_src = jvs_df.xs((src_idx, stamp))
+        # j_trgt = jvs_df.xs((trgt_idx, stamp))
+        # je_obj = objects.set_junction(name, stamp,
+        #                               (j_src.ixs, j_trgt.ixs),
+        #                               (j_src.wys, j_trgt.wys),
+        #                               (j_src.zeds, j_trgt.zeds),
+        #                               edge_color)
+
+
+
 
 
 
--- blender_draw/eptm_drawer/objects.py
+++ blender_draw/eptm_drawer/objects.py
@@ -6,20 +6,45 @@ def hex_to_rgb(col, factors = 255.):
     return tuple(c / factors for c in bytes.fromhex(col.replace('#', '')))
 
 
+time_dilation = 10
 
-def set_cell(name, x, y, z,
+def set_jv(name, stamp, x, y, z):
+    """
+
+    """
+    obj = bpy.data.objects.get(name)
+    if obj  is None:
+        bpy.ops.object.empty_add()
+        obj = bpy.context.object
+        obj.name = name
+        obj.layers = tuple(i == 2 for i in range(20))
+    scn = bpy.context.scene
+    scn.frame_current = stamp * time_dilation
+    obj.location = (x, y, z)
+    obj.keyframe_insert('location')
+
+
+def set_cell(name, x, y, z, stamp,
              j_xx, j_yy, j_zz, color):
     """
     """
 
+    obj = bpy.data.objects.get(name)
+    if obj  is None:
+        me = bpy.data.meshes.new("CellMesh")
+        obj = bpy.data.objects.new(name, me)
+        bpy.context.scene.objects.link(obj)
+        obj.layers = tuple(i == 1 for i in range(20))
+
+    scn = bpy.context.scene
+    scn.frame_current = stamp * time_dilation
+
+    obj.location = (x, y, z)
     vertices = ([(0, 0, 0),]
                 + [(jx, jy, jz) for jx, jy, jz in
                    zip(j_xx, j_yy, j_zz)]
                 + [(j_xx[0], j_yy[0], j_zz[0])])
     faces = [(0, i + 1, i + 2, 0) for i in range(len(vertices) - 2)]
-    me = bpy.data.meshes.new("CellMesh")
-    obj = bpy.data.objects.new(name, me)
-    obj.location = (x, y, z)
     me.from_pydata(vertices, [], faces)
     me.update(calc_edges=True)
 
@@ -28,8 +53,6 @@ def set_cell(name, x, y, z,
     # bpy.ops.mesh.primitive_ico_sphere_add(subdivisions=2,
     #                                       size=1, view_align=False,
     # enter_editmode=False, location=(x, y, z), rotation=(0, 0, 0))
-    bpy.context.scene.objects.link(obj)
-    obj.layers = tuple(i == 1 for i in range(20))
 
     # material = bpy.data.materials.new("%s_color" % name)
     # material.diffuse_color = hex_to_rgb(color)
@@ -38,9 +61,56 @@ def set_cell(name, x, y, z,
     # slot.material = material
     return obj
 
-def set_junction(name, xx, yy, zz, color):
+def set_junction_arm(name, src_idx, trgt_idx):
+    '''
+    '''
+    if name in bpy.data.objects:
+        return
+    bpy.ops.object.armature_add()
+    arm = bpy.context.object
+    arm.location = (0, 0, 0)
+    arm.name = name
+    arm.parent = bpy.data.objects['jv{}'.format(src_idx)]
+    bone = arm.pose.bones[0]
+    bpy.ops.object.posemode_toggle()
+
+    bpy.ops.pose.constraint_add(type='STRETCH_TO')
+    bone.constraints["Stretch To"].target = bpy.data.objects['jv{}'.format(trgt_idx)]
+    bone.constraints["Stretch To"].rest_length = 1.
+    bone.constraints["Stretch To"].keep_axis = 'PLANE_Z'
+    bone.constraints["Stretch To"].volume = 'VOLUME_XZX'
+
+    bpy.ops.mesh.primitive_cylinder_add(vertices=8, radius=0.1,
+                                        view_align=False,
+                                        enter_editmode=False,
+                                        depth=1,
+                                        location=(0, 0, 0))
+
+    cyl_name = 'cyl{}to{}'.format(src_idx, trgt_idx)
+    obj = bpy.context.object
+    obj.name = cyl_name
+    obj.parent = bpy.data.objects[name]
+    obj.parent_type = 'BONE'
+    obj.parent_bone = "Bone"
+
+
+def set_junction(name, stamp, xx, yy, zz, color):
     """
     """
+    scn = bpy.context.scene
+    scn.frame_current = stamp * time_dilation
+    obj = bpy.data.objects.get(name)
+    if obj  is None:
+        bpy.ops.mesh.primitive_cylinder_add(vertices=8, radius=0.1,
+                                            view_align=False,
+                                            enter_editmode=False,
+                                            depth=1,
+                                            location=(0, 0, 0))
+        obj = bpy.context.object
+        obj.name = name
+        obj.rotation_mode = 'AXIS_ANGLE'
+        bpy.ops.object.origin_set(type='GEOMETRY_ORIGIN',
+                                  center='MEDIAN')
 
     x0, x1 = xx
     y0, y1 = yy
@@ -49,39 +119,16 @@ def set_junction(name, xx, yy, zz, color):
     dy = y1 - y0
     dz = z1 - z0
     depth = np.sqrt(dx**2 + dy**2 + dz**2)
-
-    bpy.ops.mesh.primitive_cylinder_add(vertices=8, radius=0.1,
-                                        view_align=False,
-                                        enter_editmode=False,
-                                        depth=depth,
-                                        location=(0, 0, 0))
-    obj = bpy.context.object
-    obj.name = name
-    # obj.dimensions = (1, 1, 1)
-
-    # obj.rotation_mode = 'ZYX'
-    # alpha = np.arctan2(dx, dy)# -
-    # beta = 0#np.arctan2(dz, np.sqrt(dx**2 + dy**2))
-    # gamma = 0#np.pi -  np.arctan2(dx, dy)
-    # rotation = (alpha, beta, gamma)
-    # obj.rotation_euler = rotation
-
-    # obj.rotation_mode = 'QUATERNION'
-    # quaternion = (dz, dy, -dx, 0)
-    # obj.rotation_quaternion = quaternion
-
-    obj.rotation_mode = 'AXIS_ANGLE'
+    obj.scale = (1, 1, depth)
+    obj.keyframe_insert('scale')
     axis_angle = (np.arctan2(np.sqrt(dx**2 + dy**2), dz), -dy, dx, 0)
     obj.rotation_axis_angle = axis_angle
-
-
-    bpy.ops.object.origin_set(type='GEOMETRY_ORIGIN',
-                              center='MEDIAN')
-
+    obj.keyframe_insert('rotation_axis_angle')
     location = ((x0 + x1)/2.,
                 (y0 + y1)/2.,
                 (z0 + z1)/2.)
     obj.location = location
+    obj.keyframe_insert('location')
     material = bpy.data.materials.new("%s_color" % name)
     material.diffuse_color = hex_to_rgb(color)
     bpy.ops.object.material_slot_add()
